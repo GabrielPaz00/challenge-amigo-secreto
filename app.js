0eea5abca052e8c24ffcc7897f078825cde62979
Gabriel Paz <gabriel.paz.190@gmail.com>
2025-08-13 20:03:43 -0600

    feat: update result display logic and styles for improved visibility

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const enableInputs = (enable) => {
   if (!enable) {
     addButton.classList.add('disable');
     nameInput.removeEventListener('keypress', handleKeyEnterPress);
+    result.classList.remove('result--hidden');
     clearList();
   } else {
     addButton.classList.remove('disable');
